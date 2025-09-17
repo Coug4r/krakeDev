e1852ba43450aef17321be4cc2fe6bd2a600cdd0
Coug4r <burneofrancisco2005@gmail.com>
2025-09-17 16:34:28 -0500

    Se agrega el boton limpiar en el html y la funcion para el mismo

--- puntoDeVenta_v4/puntoDeVenta.js
+++ puntoDeVenta_v4/puntoDeVenta.js
@@ -91,5 +91,20 @@ calcularValorTotal = function () {
             Valor a pagar por 20 cerveza corona con 10% de descuento: USD 48.75
         Si funciona, hacer un commit
     */
-                   
+limpiar = function () {
+    /*
+        Dejar todas las cajas de texto con el valor cadena vacía, 0 ó 0.0 según el tipo de dato
+        Dejar todos los textos de los montos con el valor 0.0
+        Si funciona, hacer un commit
+     */
+    mostrarTextoEnCaja('txtProducto', '');
+    mostrarTextoEnCaja('txtCantidad', '0');
+    mostrarTextoEnCaja('txtPrecio', '0.0');
+    mostrarTextoEnCaja('txtPorcentajeDescuento', '0');
+    mostrarTexto('lblSubtotal', '0.0');
+    mostrarTexto('lblDescuento', '0.0');
+    mostrarTexto('lblValorIVA', '0.0');
+    mostrarTexto('lblTotal', '0.0');
+    mostrarTexto('lblResumen', '');
+}                   
 }
