b53febffa90f7347dde258a18a01e336d62a7d51
Coug4r <burneofrancisco2005@gmail.com>
2025-09-17 16:12:03 -0500

    Se separa las funciones basicas al archivo utilitarios.js

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -14,33 +14,3 @@ saludar=function(){
     //Mostrar texto en caja
     let textoCaja = mostrarTextoEnCaja('txtNombre', '')
 }
-
-recuperarTexto=function(idComponente){
-    let componente = document.getElementById(idComponente);
-    let valorIngresado = componente.value;
-    return valorIngresado;
-}
-recuperarInt=function(idComponente){
-    let texto = recuperarTexto(idComponente);
-    let int = parseInt(texto);
-    return int;
-}
-recuperarFloat=function(idComponente){
-    let texto = recuperarTexto(idComponente);
-    let float = parseFloat(texto);
-    return float;
-}
-mostrarTexto=function(idComponente, msg){
-    let cmpTexto = document.getElementById(idComponente);
-    let texto = cmpTexto.innerText = msg;
-    return texto;
-}
-mostrarImagen=function(idComponente, rutaImagen){
-    let componente = document.getElementById(idComponente);
-    let imagen = componente.src = rutaImagen;
-}
-mostrarTextoEnCaja=function(idComponente, msg){
-    let cmpTexto = document.getElementById(idComponente);
-    let texto = cmpTexto.value = msg;
-    return texto;
-}
