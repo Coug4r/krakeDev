5c829961d20cfa1788b2edd314fdf71f550c2e8f
Coug4r <burneofrancisco2005@gmail.com>
2025-09-23 15:15:45 -0500

    Se actualiza el proyecto promedioNotas a V2 actualizando nuevas funciones con mas condicionales y gifs

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -5,10 +5,20 @@ calcularPromedioNotas=function(){
     let promedio = calcularPromedio(nota1, nota2, nota3);
     let msgPromedio = promedio.toFixed(2);
     mostrarTexto('lblPromedio', msgPromedio);
-    if (promedio>7){
+    if(promedio<5 && promedio > 0){
+        mostrarTexto('lblRespueta', 'REPROBADO')
+        mostrarImagen('imagenPromedio', './imagenes/verdrietig-sad.gif')
+    }
+    else if(promedio>=5 && promedio<=8){
+        mostrarTexto('lblRespueta', 'BUEN TRABAJO')
         mostrarImagen('imagenPromedio', './imagenes/GIFTEXITO.gif')
     }
+    else if(promedio>8 && promedio<=10){
+        mostrarTexto('lblRespueta', 'EXCELENTE!')
+        mostrarImagen('imagenPromedio', './imagenes/EXCELENTE.gif')
+    }
     else{
+        mostrarTexto('lblRespueta', 'DATOS INCORRECTOS!')
         mostrarImagen('imagenPromedio', './imagenes/INCORRETO.gif')
     }
-}
+}   
