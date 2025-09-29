ed3c59eb742cd76221e1bac9da461d7381730bbd
Coug4r <burneofrancisco2005@gmail.com>
2025-09-29 11:30:43 -0500

    Se agrega la hoja de estilos y en la funcion validarPlaca se agrega un condicional para mostrar la validez

--- placas/placas.js
+++ placas/placas.js
@@ -2,4 +2,9 @@ validarPlaca=function(){
     limpiar();
     let placa = recuperarTexto('txtPlaca');
     let erroresEstructura =  validarEstructura(placa);
+    if (erroresEstructura == true){
+        mostrarTexto('lblValidez', "PLACA NO VALIDA");
+    }else{
+        mostrarTexto('lblValidez', "PLACA VALIDA");
+    }
 }
