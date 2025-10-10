4e10be265392b39d133b9022cdc780c4e83e328d
Coug4r <burneofrancisco2005@gmail.com>
2025-10-10 11:34:43 -0500

    Se agrega la funcion mostrarResusltados y una tabla html por medio de js para mostrarlos al usuario

--- aleatorios/aleatorios.js
+++ aleatorios/aleatorios.js
@@ -1,10 +1,10 @@
-aleatorios=function(){
+randomNum =function(){
     let aleatorio;
     let aleatorioMultiplicado;
     let aleatorioEntero;
     let numeroFinal;
     aleatorio=Math.random();
-    aleatorioMultiplicado=aleatorio*100;
+    aleatorioMultiplicado=aleatorio*6;
     aleatorioEntero=parseInt(aleatorioMultiplicado);
     numeroFinal=aleatorioEntero+1;
     return numeroFinal;
@@ -13,6 +13,19 @@ generarAleatorios=function(){
     let aleatorios = [];
     let indice = recuperarInt('txtIndice');
     for (let i=0;i<indice;i++){
-        console.log(i);
+        aleatorios.push(randomNum());
     }
+    mostrarResultados(aleatorios);
+}
+mostrarResultados=function(arregloNumeros){
+    let cmpTabla = document.getElementById('divTabla');
+    let contenidoTabla='<table><tr><th>RESULTADOS</th></tr>';
+    for(let i=0;i<arregloNumeros.length;i++){
+        let resultado = arregloNumeros[i];
+        contenidoTabla+="<tr><td>";
+        contenidoTabla+= resultado;
+        contenidoTabla+="</tr></td>";
+    }
+        contenidoTabla+="</table>";
+    cmpTabla.innerHTML=contenidoTabla;
 }
