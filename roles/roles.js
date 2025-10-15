022234bc5d2331fa145757b2246396fbf89d5606
Coug4r <burneofrancisco2005@gmail.com>
2025-10-14 22:07:43 -0500

    Se agrega la funcion mostrar empleados y se da un estilo nuevo a la tabla de empleados

--- roles/roles.js
+++ roles/roles.js
@@ -1,11 +1,13 @@
 let empleados = [
     {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0},
+    {cedula:"0915622729",nombre:"Rafael",apellido:"Correa",sueldo:200.0,}
 ]
 mostrarOpcionEmpleado=function(){
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
+    mostrarEmpleados();
 }
 mostrarOpcionRol=function(){
     mostrarComponente("divRol");
@@ -17,4 +19,24 @@ mostrarOpcionResumen=function(){
     ocultarComponente("divRol")
     ocultarComponente("divEmpleado");
 }
-
+mostrarEmpleados=function(){
+    let cmpTabla = document.getElementById("tablaEmpleados");
+    let contenidoTabla = "<table> <tr>"
+    + "<th>CEDULA</th>" 
+    + "<th>NOMBRE</th>"
+    + "<th>APELLIDO</th>"
+    + "<th>SUELDO</th>"
+    +"</tr>";
+    let elementoEmpleados;
+    for(let i=0; i<empleados.length;i++){
+        elementoEmpleados=empleados[i];
+        contenidoTabla+=
+        "<tr><td>"+ elementoEmpleados.cedula +"</td>"
+        + "<td>"+ elementoEmpleados.nombre + "</td>" 
+        + "<td>"+ elementoEmpleados.apellido + "</td>"
+        + "<td>"+ elementoEmpleados.sueldo + "</td>"
+        +"</tr>";
+    }
+    contenidoTabla+="</table>";
+    cmpTabla.innerHTML=contenidoTabla;
+}
